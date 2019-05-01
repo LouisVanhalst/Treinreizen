ab7e94b117b79672114e807e25a011b01bd59917
Sophie De Waele <sophie.dewaele@student.vives.be>
2019-05-01 15:37:42 +0200

    mijn eerst commit

--- Treinreizen/Controllers/HomeController.cs
+++ Treinreizen/Controllers/HomeController.cs
@@ -40,4 +40,6 @@ namespace Treinreizen.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+
+    //SDW
 }
