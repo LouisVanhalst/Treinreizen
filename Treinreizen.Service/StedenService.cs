f12dafb6773ec27d1dcb62f81fa41d28e8853434
Sophie De Waele <sophie.dewaele@student.vives.be>
2019-05-01 16:45:47 +0200

    winkelkarlogo + lijst van steden

---
+++ Treinreizen.Service/StedenService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Treinreizen.Domain.Entities;
+using Treinreizen.Storage;
+
+namespace Treinreizen.Service
+{
+    public class StedenService
+    {
+        private StedenDAO stedenDAO;
+
+        public StedenService()
+        {
+            stedenDAO = new StedenDAO();
+        }
+
+        public IEnumerable<Steden> GetAll()
+        {
+            return stedenDAO.GetAll();
+        }
+    }
+}
--- Treinreizen/Controllers/TreinController.cs
+++ Treinreizen/Controllers/TreinController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Treinreizen.Service;
 using Treinreizen.ViewModel;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,10 +13,13 @@ namespace Treinreizen.Controllers
 {
     public class TreinController : Controller
     {
+        private StedenService stedenService;
+
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Home()
         {
+            //ViewBag.lstSteden = new SelectList(stedenService.GetAll(),"StadId","Naam");
             return View();
         }
 
@@ -26,8 +31,10 @@ namespace Treinreizen.Controllers
         // GET: /<controller>/
         public IActionResult Wie()
         {
-            return View();
+            var list = stedenService.GetAll();
+            return View(list);
         }
+
         public IActionResult Wat()
         {
             return View();
@@ -36,6 +43,7 @@ namespace Treinreizen.Controllers
         {
             return View();
         }
-    
+           
+       
 }
 }
